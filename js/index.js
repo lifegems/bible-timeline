80aee13940f0a474501fd7d68c1a9595805aec99
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-03-25 21:00:42 +0000

    Add dependencies locally

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var ctrl = new ScrollMagic.Controller();
 
-function initDateline() {   
+function initDateline() {
    var startDate = 0;
    addNewDate("In the beginning..", "");
    for (var i = 4100; i > 0; i = i - 100) {
@@ -24,7 +24,7 @@ function shiftDatesUp(strTriggerName) {
       )
       //.addIndicators()
       .addTo(ctrl);
-   
+
    new ScrollMagic
       .Scene({
          triggerElement: strTriggerName,
@@ -35,12 +35,12 @@ function shiftDatesUp(strTriggerName) {
       )
       //.addIndicators()
       .addTo(ctrl);
-   
+
    new ScrollMagic
       .Scene({
          duration: 10000,
          triggerElement: strTriggerName
-      })   
+      })
       .setTween(
          TweenMax.to(".info", 1, {x: "-=5000", ease: Linear.easeNone})
       )
@@ -51,13 +51,13 @@ function addNewDate(intNewDate, strSuffix) {
    $linegroup = $('.dateline-md');
    intLength = $linegroup.length + 1;
    pctLeftValue = (20 * intLength) + "%";
-   
+
    // main year number
    var $newDate = $('<div></div>', {class: "dateline-md"});
    $newDate.text(intNewDate + strSuffix);
    $newDate.css("left", pctLeftValue);
    $('.linegroup').append($newDate);
-   
+
    // incremental years
    for (var i = 1; i < 11; i = i + 1) {
       var $smLine = $('<div></div>', {class: "dateline-sm"});
@@ -80,7 +80,7 @@ function showTimelineTitle() {
    $titleNote  = $('<div></div>', {id: 'titleNote'});
    $titleNote.html("<h1>The Bible Timeline</h1>");
    $notes.append($titleNote);
-   
+
    $timeline.prepend($notes);
    TweenMax.fromTo("#titleNote", 2, {alpha: 0, x: 800, y: 450}, {alpha: 1, ease: Linear.easeNone});
 }
@@ -91,7 +91,7 @@ function showScrollText() {
    $scrollNote = $('<div></div>', {id: 'scroll'});
    $scrollNote.html("<h3>Scroll <span class='fa fa-chevron-down'></span> to move <span class='fa fa-arrow-right'></span></h3>");
    $notes.append($scrollNote);
-   
+
    $timeline.prepend($notes);
    TweenMax.fromTo("#scroll", 2, {y: "0", x: "800"}, {y: "600", ease: Bounce.easeOut});
 }
@@ -119,13 +119,13 @@ $('#expand').on('click', function(e) {
    if (blLegendExpanded === false) {
       blLegendExpanded = true;
       $(this).css('background-color', '#888');
-      TweenMax.fromTo("#key", 1, {y: 0}, {y: 375, alpha: 1, onComplete:setLegendIcon});  
+      TweenMax.fromTo("#key", 1, {y: 0}, {y: 375, alpha: 1, onComplete:setLegendIcon});
    } else {
       blLegendExpanded = false;
-      TweenMax.to("#key", 0, {alpha: 0}); 
+      TweenMax.to("#key", 0, {alpha: 0});
       $(this).css('background-color', '#ccc');
       $(this).css('color', '#000');
-      $("#expand").html('<span class="fa fa-list-ul"></span>'); 
+      $("#expand").html('<span class="fa fa-list-ul"></span>');
    }
 });
 
@@ -139,7 +139,7 @@ $('#settings-btn').on('click', function(e) {
    if (blSettingsOpen === false) {
       blSettingsOpen = true;
       $(this).css('background-color', '#888');
-      TweenMax.fromTo("#settings-menu", 1, {x: -500}, {alpha: 1, x: 25, onComplete:setSettingsIcon});      
+      TweenMax.fromTo("#settings-menu", 1, {x: -500}, {alpha: 1, x: 25, onComplete:setSettingsIcon});
    } else {
       blSettingsOpen = false;
       TweenMax.to("#settings-menu", 0, {alpha: 0});
@@ -155,10 +155,10 @@ $('#tglKings').on('change', function(e) {
    console.log("toggle the kings");
    if (blShowKings === true) {
       blShowKings = false;
-      $('#kings').hide();      
+      $('#kings').hide();
    } else {
       blShowKings = true;
-      $('#kings').show();      
+      $('#kings').show();
    }
 });
 
@@ -167,10 +167,10 @@ $('#tglBblComp').on('change', function(e) {
    console.log("toggle the kings");
    if (blShowBblComp === true) {
       blShowBblComp = false;
-      $('#bblComp').hide();      
+      $('#bblComp').hide();
    } else {
       blShowBblComp = true;
-      $('#bblComp').show();      
+      $('#bblComp').show();
    }
 });
 
@@ -179,9 +179,9 @@ $('#tglBblCov').on('change', function(e) {
    console.log("toggle the kings");
    if (blShowBblCov === true) {
       blShowBblCov = false;
-      $('#bblCov').hide();      
+      $('#bblCov').hide();
    } else {
       blShowBblCov = true;
-      $('#bblCov').show();      
+      $('#bblCov').show();
    }
 });
